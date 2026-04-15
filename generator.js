5a48451a06090d9992a3e57a987032e3c9d054e8
mariachauplannaz-oss <maria.chauplannaz@gmail.com>
2026-04-15 15:37:53 +0200

    block techpack en STY

--- generator.js
+++ generator.js
@@ -179,5 +179,5 @@ export function generate(state, log) {
 
     document.getElementById('topbarTitle').textContent = (DICT[selections.torso]||'T-Shirt') + ' \u2014 Generated';
     log('Done!', 'ok');
-    document.getElementById('btnTechPack').style.display = '';
+    document.getElementById('btnTechPack').style.display = cfg.free ? 'none' : '';
 }
